fae0c53c63d9b83164be3c1cb76bd6af2134892e
poketolent <b10302121@mail.ntust.edu.tw>
2020-11-12 17:05:54 +0800

    Add ProxyPattern

---
+++ Proxy/ProxyPattern/ProxyMan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.ProxyPattern
+{
+    class ProxyMan : IGiveGift
+    {
+        PursuitMan Peter;
+        public ProxyMan(SchoolGirl female)
+        {
+            Peter = new PursuitMan(female);
+        }
+        
+        public void GiveDolls()
+        {
+            Peter.GiveDolls();
+        }
+        public void GiveFlowers()
+        {
+            Peter.GiveFlowers();
+        }
+        public void GiveChocolate()
+        {
+            Peter.GiveChocolate();
+        }
+    }
+}
