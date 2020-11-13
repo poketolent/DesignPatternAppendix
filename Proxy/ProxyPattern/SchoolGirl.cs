d80ad2cc5586fb6fed555b7a9205e9753260d5ba
poketolent <b10302121@mail.ntust.edu.tw>
2020-11-13 09:26:47 +0800

    Revert "Revert "Add ProxyPattern""
    This reverts commit 49118dda26677c11f9c35ed4e5eef63ce8e9b3b7.

---
+++ Proxy/ProxyPattern/SchoolGirl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.ProxyPattern
+{
+    class SchoolGirl
+    {
+        private string name;
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
