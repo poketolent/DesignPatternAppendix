65076906a95b50a8e36a4e5c5e76e57dd9a0ca29
poketolent <b10302121@mail.ntust.edu.tw>
2020-11-13 10:41:59 +0800

    FactoryMethod 1 and Factory Method 2 Finished

---
+++ FactoryMethod/Operations/SubFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Operations
+{
+    class SubFactory : IFactory
+    {
+    public Operation CreateOperation()
+        {
+            return new OperationSub();
+        }
+    }
+}
---
+++ FactoryMethod2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod2.FactoryMethod;
+
+namespace FactoryMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory factory = new UngraduateFactory();
+            Nightingale Student = factory.CreateNightingale();
+            Student.Sweep();
+            Student.Wash();
+            Student.BuyRice();
+            Console.Read();
+        }
+    }
+}
